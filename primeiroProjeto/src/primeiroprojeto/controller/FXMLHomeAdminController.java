53ecb2cd7d580bb3c24a17d0e21ebbe8c3432e55
Raul <raulzin.raul@gmail.com>
2020-12-09 20:03:43 -0300

    tela principal

--- primeiroProjeto/src/primeiroprojeto/controller/FXMLHomeAdminController.java
+++ primeiroProjeto/src/primeiroprojeto/controller/FXMLHomeAdminController.java
@@ -43,7 +43,7 @@ public class FXMLHomeAdminController implements Initializable {
     
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        loadFuncionarios();
+//        loadFuncionarios();
     }
     
     private void loadFuncionarios() {
